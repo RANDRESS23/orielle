96325ab8e840da88db9a355ad0fb406ae8e5be49
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-11 12:36:12 -0500

    feat: create AdminLayout component and update MainLayout to comment out Header

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,32 @@
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { SearchProvider } from '@/context/search-context'
+import { cn } from '@/lib/utils'
+import { Outlet } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { AppSidebar } from '@/components/layout/app-sidebar'
+
+export const AdminLayout = () => {
+  const defaultOpen = Cookies.get('sidebar:state') !== 'false'
+
+  return (
+    <SearchProvider>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <div
+          id='content'
+          className={cn(
+            'ml-auto w-full max-w-full',
+            'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
+            'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
+            'transition-[width] duration-200 ease-linear',
+            'flex h-svh flex-col',
+            'group-data-[scroll-locked=1]/body:h-full',
+            'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
+          )}
+        >
+          <Outlet />
+        </div>
+      </SidebarProvider>
+    </SearchProvider>
+  )
+}
